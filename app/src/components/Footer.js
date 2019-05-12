47f6144a47aa44af8f5432d181ca47969bee6d54
tobyjaguar <talgya@gmail.com>
2019-05-12 19:28:43 -0400

    fix footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -33,7 +33,7 @@ const Footer = () => (
           <p className="mb-0">
             &copy; {new Date().getFullYear()} -{" "}
             <span href="/" className="text-muted">
-              AppStack
+              SmartPiggies
             </span>
           </p>
         </Col>

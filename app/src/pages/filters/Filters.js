72200a7ea8be79a37f292aa9a39a9ef8d663ad7c
tobyjaguar <talgya@gmail.com>
2019-05-12 19:03:08 -0400

    add filter component

---
+++ app/src/pages/filters/Filters.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+class Filters extends Component {
+  constructor(props, context) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+      hello
+      </div>
+    )
+  }
+}
+
+export default Filters
